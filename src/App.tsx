ba28bb38029c101d4e26011ee7a8cf12751d7e22
André Soares <codigoaberto.info@gmail.com>
2020-02-20 22:24:13 -0300

    feat: add header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter } from "react-router-dom";
 
 import GlobalStyles from "./styles/global";
-import Routes from './routes';
+import Routes from "./routes";
+
+import Header from "./components/Header";
 
 function App() {
   return (
     <BrowserRouter>
-      {/* <Header /> */}
+      <Header />
       <Routes />
       <GlobalStyles />
     </BrowserRouter>
