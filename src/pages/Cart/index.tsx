5441c07ff486dfbfb4f735dae23b92debbafabae
André Soares <codigoaberto.info@gmail.com>
2020-02-21 14:01:30 -0300

    feat: styled cart component and add service faker

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,11 +1,71 @@
 import React from "react";
+import { Container, ProductTable, Total } from "./styles";
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete
+} from "react-icons/md";
 
 const Cart = () => {
-    return (
-        <div>
-            <h1>Carrinho</h1>
-        </div>
-    )
-}
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>PRODUTO</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom1.jpg"
+                alt="Tênis"
+              />
+            </td>
+            <td>
+              <strong>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              </strong>
+              <span>R$129,90</span>
+            </td>
+            <td>
+              <div>
+                <button>
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type="number" readOnly value={2} />
+                <button>
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>R$259,80</strong>
+            </td>
+            <td>
+              <button>
+                <MdDelete size={20} color="#7159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+
+      <footer>
+        <button>Finalizar Pedido</button>
+
+        <Total>
+          <span>TOTAL</span>
+          <strong>R$ 1280,98</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
+};
 
 export default Cart;
